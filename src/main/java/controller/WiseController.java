3a84574d81e84aad4423c808a5767679f4558c75
5s-mini <dnclsehd123@naver.com>
2026-02-02 17:21:02 +0900

    feat: 입출력 처리 클래스 추가

--- src/main/java/controller/WiseController.java
+++ src/main/java/controller/WiseController.java
@@ -1,11 +1,18 @@
 package controller;
 
-public class WiseController {
-    public  WiseController() {
+import input.WiseInput;
+import view.WiseOutput;
 
+public class WiseController {
+
+    private final WiseInput wiseInput;
+
+    public  WiseController() {
+        WiseOutput wiseOutput = new WiseOutput();
+        this.wiseInput = new WiseInput();
     }
 
     public void Run() {
-
+        wiseInput.Start();
     }
 }
